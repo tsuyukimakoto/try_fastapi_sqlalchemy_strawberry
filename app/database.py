6caa1f8d7e053e9785aff1a8605725c94ad52375
makoto tsuyuki <mtsuyuki@gmail.com>
2025-04-27 12:27:49 +0900

    データベース設定

---
+++ app/database.py
@@ -0,0 +1,27 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from typing import AsyncGenerator
+
+
+SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./test.db"
+
+# 非同期エンジン
+engine = create_async_engine(SQLALCHEMY_DATABASE_URL, connection_args={"check_same_thread": False}, echo=True)
+
+
+# 非同期セッションファクトリ
+AsyncSessionLocal = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
+
+class Base(DeclarativeBase):
+  pass
+
+
+# FastAPIのDIで使用するDBセッションを取得するための関数
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSessionLocal() as session:
+        yield session
